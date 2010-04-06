41ed13042c57bbfd5d9dcce24f0f75067f572e54
oxon <oxon>
2010-04-06 01:36:10 +0000

    Update the version number to 1.1.7

--- src/setup.py
+++ src/setup.py
@@ -1,12 +1,12 @@
 """
 Setup script for PyFitsRoot
-$Id: setup.py,v 1.8 2010/04/05 05:48:59 oxon Exp $
+$Id: setup.py,v 1.9 2010/04/06 01:36:10 oxon Exp $
 """
 
 from distutils.core import setup
 
 setup(name="PyFitsRoot",
-      version="1.1.6",
+      version="1.1.7",
       description="Package for FITS interface in PyROOT",
       author="Akira Okumura",
       author_email="oxon@juno.phys.s.u-tokyo.ac.jp",
